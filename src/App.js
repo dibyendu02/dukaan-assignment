8d272ee66ad2babf22112bdbb158ae0952f80cf7
Dibyendu Das <get2dibyendu@gmail.com>
2024-01-09 17:24:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="h-min-screen">
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    // colors:{
+    //   "primary":"#1E2640",
+    //   "background":"#353C53",
+    //   "background-light":"#E6E6E6",
+    //   "text":"#4D4D4D",
+    //   "text-dark":"#1A181E",
+    //   "secondary":"#146EB4",
+    //   // "white":"#fff"
+
+    // }
+  },
+  plugins: [],
+}
+
